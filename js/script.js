4311b43559f3b07ae27541275153d8fdeefa4a2c
HenriqueHyonemoto <ifhenriquehyonemoto@gmail.com>
2024-11-30 21:51:32 -0300

    fade scroll teste

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+AOS.init();
+
 function trocaLingua() {
     const language = document.getElementById('language-switcher').value;
     document.documentElement.lang = language;
     location.reload();
     alert('A função de troca de idiomas ainda está em desenvolvimento!');
 }
+
