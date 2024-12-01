100fb98f315c1103e08c17b66a686bae3171b4dc
HenriqueHyonemoto <ifhenriquehyonemoto@gmail.com>
2024-11-30 22:42:14 -0300

    Fade Cards

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-AOS.init();
-
 function trocaLingua() {
     const language = document.getElementById('language-switcher').value;
     document.documentElement.lang = language;
@@ -7,3 +5,28 @@ function trocaLingua() {
     alert('A função de troca de idiomas ainda está em desenvolvimento!');
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const elements = document.querySelectorAll('.fade-up, .fade-down');
+    const thresholdUp = 300; // Ajuste este valor para fade-up do card
+    const thresholdDown = 400; // Ajuste este valor para fade-down do card
+    function checkVisibility() {
+        elements.forEach(element => {
+            const rect = element.getBoundingClientRect();
+            const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+            // Verifica se o elemento está dentro da viewport
+            if (rect.top <= windowHeight - thresholdUp && rect.bottom >= thresholdDown) {
+                element.classList.add('visible');
+                element.classList.remove('hidden');
+            } else {
+                    element.classList.remove('visible');
+                    element.classList.add('hidden');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', checkVisibility);
+    window.addEventListener('resize', checkVisibility);
+    checkVisibility(); // Verifica a visibilidade inicial
+});
