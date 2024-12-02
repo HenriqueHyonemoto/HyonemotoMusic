e0d4c56fae4fc91337f95f64fac55735c61eb1e7
HenriqueHyonemoto <ifhenriquehyonemoto@gmail.com>
2024-12-02 11:49:52 -0300

    sobre mim

--- myinfo.php
+++ myinfo.php
@@ -11,36 +11,33 @@
     <script src="https://kit.fontawesome.com/50812450f6.js" crossorigin="anonymous"></script>
     <?php require "font.php"?>
 </head>
+<?php 
+$dataNasc = new DateTime('2003-08-25');
 
+// Data atual
+$dataAtual = new DateTime();
+
+// Calcula a diferença entre as duas datas
+$dif = $dataAtual->diff($dataNasc);
+
+// Obtém a idade em anos
+$idade = $dif->y;
+?>
 <body class = "content-body">
 <?php require "navbar.php"?>
 <div class="content-container">
 <section class="content-section">
     <h1>Quem sou eu?</h1>
     <hr>
-    <p>Me chamo <span style="color:orange">Henrique Hyonemoto</span></p>
-</section>
-    <section class="content-section">
-        <h1>Por que esse site foi desenvolvido?</h1>
-        <hr>
-        <div class = "image-line">
-        <img src="images/content-site.jpeg" alt="Foto do código do projeto" width="700px" class="scale-transform11">
+    <div class = "image-line">
+        <img src="images/logo-h-bk.png" alt="Foto do código do projeto" width="450px" class="scale-transform11">
 </div>
-        <p>Este site foi criado como parte de um projeto autoral, com o objetivo de aprimorar minhas habilidades no desenvolvimento de aplicações web front-end. O conteúdo é original e foi pensado para estimular a criatividade, proporcionando uma experiência que combina aprendizado técnico com design e inovação.</p>
-    </section>
+<p>Me chamo <strong>Henrique Hyonemoto</strong>, tenho atualmente <?= $idade ?> anos. Iniciei meus estudos em programação em 2021, aos 17 anos, no <strong>Instituto Federal de Educação, Ciência e Tecnologia de São Paulo, Campus Votuporanga</strong>, onde atualmente estou no 8º semestre.</p> <p>Ao longo da minha trajetória, realizei projetos de estudo para aprimorar meus conhecimentos em desenvolvimento web, utilizando <strong>HTML, CSS, JavaScript e Bootstrap</strong> no front-end, e <strong>PHP, Laravel e MySQL</strong> no back-end. Também explorei a integração de sistemas embarcados com <strong>Arduino</strong> e C++, abordando tanto interfaces quanto o controle de hardware.</p> 
+<p>Sou também um entusiasta da música, tendo <strong>iniciado meus estudos no piano em 2020</strong>, após ser apresentado às obras clássicas de Chopin e Beethoven. Desenvolvi-me rapidamente e, em pouco tempo, comecei a compor peças autorais. <strong> Em Setembro de 2024, comecei a aprender Violão</strong>, influenciado pela banda Nirvana.</p>
+</section>
+    
 
-    <section class="content-section">
-        <h1>O que foi utilizado na construção desse site?</h1>
-        <hr>
-        <div class = "image-line">
-        <img src="images/content-html.png" alt="Icones de HTML, CSS, JavaScript, PHP e Photoshop" width="100px" class="scale-transform15">
-        <img src="images/content-css.png" alt="Icones de HTML, CSS, JavaScript, PHP e Photoshop" width="100px" class="scale-transform15">
-        <img src="images/content-js.png" alt="Icones de HTML, CSS, JavaScript, PHP e Photoshop" width="100px" class="scale-transform15">
-        <img src="images/content-php.png" alt="Icones de HTML, CSS, JavaScript, PHP e Photoshop" width="100px" class="scale-transform15">
-        <img src="images/content-photoshop.png" alt="Icones de HTML, CSS, JavaScript, PHP e Photoshop" width="100px" class="scale-transform15">
-        </div>
-        <p>Para a construção desse site foi utilizado <strong>HTML, CSS, JavaScript, PHP</strong> e <strong>Photoshop</strong> para edição e personalização de imagens. Também foram utilizados ícones da <a href="https://fontawesome.com/search">FontAwesome</a> e <a href="https://lucide.dev/icons/">Lucide Icons</a>.</p>
-    </section>
+   
 </div>
 <?php require "footer.php"?>
     <script src="https://unpkg.com/lucide@latest"></script>
